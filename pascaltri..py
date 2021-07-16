d6e0a49c11833a47b6b6d89997f4aa8d90da21c6
KowsalyaS18 <87513120+KowsalyaS18@users.noreply.github.com>
2021-07-16 13:34:04 +0530

    Create pascaltri..py
    Given numRows = 5,.
    
    Generate
    A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1
    [
    [1],
    [1,1],
    [1,2,1],
    [1,3,3,1],
    [1,4,6,4,1]
    ]

---
+++ pascaltri..py
@@ -0,0 +1,14 @@
+from math import factorial
+
+n =int(input("enter the number"))
+for i in range(n):
+    for j in range(n-i+1):
+ 
+        
+        print(end=" ")
+ 
+    for j in range(i+1):
+ 
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+ 
+    print()
